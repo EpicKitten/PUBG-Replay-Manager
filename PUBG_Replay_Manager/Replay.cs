89d93ca34f57c08d632c27896e7b8c404bccd669
aaa4xu <aaa4xu@gmail.com>
2018-02-26 16:58:43 +0300

    Refactoring replay files parsing

---
+++ PUBG_Replay_Manager/Replay.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace PUBG_Replay_Manager
+{
+    public class Replay
+    {
+        private string Path;
+
+        public ReplayInfo Info;
+        public ReplaySummary Summary;
+        
+        public Replay(string path)
+        {
+            Path = path;
+            Info = DecryptReplayInfoFile();
+            Summary = DecryptReplaySummaryFile();
+            
+            Console.Write("");
+        }
+
+        private ReplaySummary DecryptReplaySummaryFile()
+        {
+            string filename = null;
+            
+            foreach (string replay in Directory.GetFiles(Path + "\\data"))
+            {
+                if (replay.Contains("ReplaySummary"))
+                {
+                    filename = replay;
+                }
+            }
+
+            if (filename == null)
+            {
+                throw new Exception("ReplaySummary file is not found");
+            }
+            
+            string summaryJson = Utils.UE4StringSerializer(filename, 1);
+            return JsonConvert.DeserializeObject<ReplaySummary>(summaryJson);
+        }
+
+        private ReplayInfo DecryptReplayInfoFile()
+        {
+            string infoJson = Utils.UE4StringSerializer(Path + "\\PUBG.replayinfo");
+            return JsonConvert.DeserializeObject<ReplayInfo>(infoJson);
+        }
+    }
+}
